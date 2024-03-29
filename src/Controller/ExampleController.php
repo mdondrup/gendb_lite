15e5684ee325d29b9248065cbf809937cf304f2f
Michael Dondrup <mdondrup@users.noreply.github.com>
2024-03-29 16:44:40 +0100

    Update ExampleController.php

--- src/Controller/ExampleController.php
+++ src/Controller/ExampleController.php
@@ -8,7 +8,7 @@ use Drupal\Core\Controller\ControllerBase;
  */
 class ExampleController extends ControllerBase {
 
-private function gene_count() {
+ function gene_count() {
 
   $sql = "SELECT count(*) FROM chado.feature AS f LEFT JOIN chado.f_type AS t ON f.type_id = t.type_id WHERE t.type = 'gene'";
   $result = \Drupal::database()->query($sql);
@@ -23,7 +23,7 @@ private function gene_count() {
    */
   public function myPage() {
     return [
-      '#markup' => 'Checking for genes in Chado...</br> Found '. this.gene_count() .' genes.',
+      '#markup' => 'Checking for genes in Chado...</br> Found '. gene_count() .' genes.',
     ];
   }
 
