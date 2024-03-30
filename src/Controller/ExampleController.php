10d05df4bbe743552d0bde61bd814444eb7ad249
Michael Dondrup <mdondrup@users.noreply.github.com>
2024-03-30 10:29:29 +0100

    Update ExampleController.php

--- src/Controller/ExampleController.php
+++ src/Controller/ExampleController.php
@@ -3,13 +3,13 @@ namespace Drupal\gendb_lite\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+* Counts the number of gene entries in Chado DB, should be move to utility functions
+*/
 
  function gene_count() {
-
   $sql = "SELECT COUNT(DISTINCT f.feature_id) FROM chado.feature AS f LEFT JOIN chado.f_type AS t ON f.type_id = t.type_id WHERE t.type = 'gene'";
-  $query = \Drupal::database()->query($sql);
-  
-  
+  $query = \Drupal::database()->query($sql); 
   return($query->fetch()->count);
 }
 
@@ -29,7 +29,7 @@ class ExampleController extends ControllerBase {
    */
   public function myPage() {
     return [
-      '#markup' => 'Checking for genes in Chado...</br> Found '. gene_count() .' genes.',
+      '#markup' => 'Counting genes in Chado...</br> Found '. gene_count() .' genes.',
     ];
   }
 
